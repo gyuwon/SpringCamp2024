0cf4c878240c6e92da2aefb63ceba75119bd69b2
Gyuwon Yi <gyuwon@live.com>
2024-04-28 18:58:35 +0900

    프로젝트를 초기화한다

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.2.5"
+    id("io.spring.dependency-management") version "1.1.4"
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    runtimeOnly("com.h2database:h2")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/main/java/wiredcommerce/CommerceApplication.java
@@ -0,0 +1,12 @@
+package wiredcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommerceApplication.class, args);
+    }
+}
